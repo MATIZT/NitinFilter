5841dc6289e78a3dbeea0cd9a689b963fb5fd263
MATIZT <147545725+MATIZT@users.noreply.github.com>
2024-06-05 14:52:26 +0530

    Update share_text.py

--- plugins/modules/share_text.py
+++ plugins/modules/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/LUCI_MOVIES")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/MoviesFlicker")]])
             )                                                   
         return
     await message.reply_text(
